5d141b34e9297f65a580b51b500e4e736ff0bec5
Strider White <strider.white@pderas.com>
2023-04-09 18:20:00 -0600

    basic idea

---
+++ resources/js/Components/TitleBadge.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <span class="bg-red text-white px-2 text-2xl">
+            {{ title }}
+        </span>
+    </div>
+</template>
+
+  <script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  })
+  </script>
