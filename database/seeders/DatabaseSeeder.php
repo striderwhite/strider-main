e82f565029d70334a7fa5654fc7be82063a325cc
Strider White <strider.white@pderas.com>
2023-04-10 12:50:35 -0600

    remove register route and added user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::firstOrCreate(
+            ['email' => 'strider@striderwhite.com'],
+            [
+                'email' => 'strider@striderwhite.com',
+                'name' => 'Strider',
+                'password' => env('PASSWORD_SEED')
+            ]
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
